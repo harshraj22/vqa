7b4b5f0b583891a8d4aa5a90dd9f4d7b531b6d5d
Harsh Raj <janhavi2021@cares3.iitdh.ac.in>
2021-09-11 11:52:02 +0530

    added some unit tests for dataset

--- utils/dataset.py
+++ utils/dataset.py
@@ -9,6 +9,7 @@ import torchvision.transforms as T
 import numpy as np
 import json
 from transformers import BertTokenizer, DistilBertTokenizer, BertTokenizerFast
+import unittest
 
 class MultiImageVQADataset(Dataset):
     """one image from clever and other 3 image from other.
@@ -86,18 +87,63 @@ class MultiImageVQADataset(Dataset):
         
         return dct
 
+
+class TestDataset(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.num_random_images = 3
+        print(f'Creating the dataset......')
+        self.data = MultiImageVQADataset('/nfs_home/janhavi2021/clever/CLEVR_v1.0/questions/CLEVR_val_questions.json', '/nfs_home/janhavi2021/clever/CLEVR_v1.0/images/val', '/nfs_home/janhavi2021/Tiny/tiny-imagenet-200/test/images', randomImages=self.num_random_images)
+        print(f'Created the dataset....')
+
+    def test_dct_len(self):
+        # num of points in dataset should be non-zero
+        self.assertTrue(len(self.data) > 0)
+
+    def test_return_type(self):
+        # the objects of dataset should be dictionaries
+        dct = self.data[0]
+        self.assertEqual(type(dct), type(dict()))
+
+    def test_dct_keys(self):
+        # some keys must be present in the dictionary.
+        # these keys are used in the training loop, and the model
+        dct = self.data[0]
+        required_keys = ['images', 'ans', 'ques', 'true_img_index']
+        self.assertTrue(all(key in dct.keys() for key in required_keys))
+
+    def test_num_images(self):
+        dct = self.data[0]
+        self.assertEqual(len(dct['images']), self.num_random_images + 1)
+
+    def test_one_word_answer(self):
+        # the answer returned, should be of one word
+        dct = self.data[0]
+        self.assertEqual(tuple(dct['ans'].shape), (1,))
+
+    def test_answer_dtype(self):
+        # answer should be torch.LongTensor
+        dct = self.data[0]
+        self.assertEqual(dct['ans'].dtype, torch.int64)
+
+    def test_ques_dtype(self):
+        # ques should be torch.LongTensor
+        dct = self.data[0]
+        self.assertEqual(dct['ques'].dtype, torch.int64)
+
 if __name__ == '__main__':
-    data = MultiImageVQADataset('/nfs_home/janhavi2021/clever/CLEVR_v1.0/questions/CLEVR_val_questions.json', '/nfs_home/janhavi2021/clever/CLEVR_v1.0/images/val', '/nfs_home/janhavi2021/Tiny/tiny-imagenet-200/test/images')
-    d = data.__getitem__(2)
-    print(d['images'][0].shape, len(d['images']))
-    print(d["ques"])
-    print(d["ans"])
+    unittest.main()
+    # data = MultiImageVQADataset('/nfs_home/janhavi2021/clever/CLEVR_v1.0/questions/CLEVR_val_questions.json', '/nfs_home/janhavi2021/clever/CLEVR_v1.0/images/val', '/nfs_home/janhavi2021/Tiny/tiny-imagenet-200/test/images')
+    # d = data.__getitem__(2)
+    # print(d['images'][0].shape, len(d['images']))
+    # print(d["ques"])
+    # print(d["ans"])
 
-    print(len(d))
+    # print(len(d))
 
-    for i in range(1000):
-        # print(len(d['images']), len(d['ques']), len(d['ans']))
-        for key, val in d.items():
-            print(key, len(val), end=' ')
-        print()
+    # for i in range(1000):
+    #     # print(len(d['images']), len(d['ques']), len(d['ans']))
+    #     for key, val in d.items():
+    #         print(key, len(val), end=' ')
+    #     print()
 
