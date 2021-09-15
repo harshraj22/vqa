23e0907c580f9fb19a47efff2c3d1258ff6d0c6a
Harsh Raj <janhavi2021@cares3.iitdh.ac.in>
2021-09-15 22:19:19 +0530

    code for tracing word/ img_classification accuracy

--- models/train.py
+++ models/train.py
@@ -6,7 +6,9 @@ from torch.optim.lr_scheduler import ReduceLROnPlateau
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 import random
+import json
 from math import inf
+from sklearn.metrics import accuracy_score
 
 import sys
 sys.path.append("..")
@@ -24,15 +26,11 @@ feat_dim = 640 # from paper, the final vector vq, vi
 embed_size = 500 # from paper, dimention of embedding of each word
 n_attention_stacks = 2
 hidden_dim_img = feat_dim
-batch_size = 8
+batch_size = 40
 lambda_ = 5
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-model = MultiImageVQA(feat_dim, vocab_size, embed_size, n_attention_stacks, hidden_dim_img)
-criterian = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters())
-scheduler = ReduceLROnPlateau(optimizer, verbose=True, patience=6)
-
+answers = ['6', 'yes', 'small', 'blue', 'brown', 'red', 'cyan', '1', '0', 'rubber', '7', 'no', 'purple', 'large', '5', 'green', '10', 'cube', '4', '9', '3', 'cylinder', 'metal', '2', 'gray', 'sphere', '8', 'yellow']
 
 class Multi(Dataset):
     def __init__(self):
@@ -55,10 +53,26 @@ class Multi(Dataset):
 ds = MultiImageVQADataset('/nfs_home/janhavi2021/vqa/models/cleaned.json', '/nfs_home/janhavi2021/clever/CLEVR_v1.0/images/val', '/nfs_home/janhavi2021/Tiny/tiny-imagenet-200/test/images')
 datasetLength = len(ds)
 
+# answer_tokens = ds.tokenizer.tokenize(answers)
+# answer_indices = ds.tokenizer.convert_tokens_to_ids(answer_tokens)
+# crossentropy_weights = [0 for _ in range(30000)]
+# for answer_index in answer_indices:
+#     crossentropy_weights[answer_index] = 1 / len(answer_indices)
+
 print("Dataset Loaded. ", datasetLength)
-train_, validation_ = torch.utils.data.random_split(ds, [int(0.9*datasetLength), len(ds) - int(0.9*datasetLength)], generator=torch.Generator().manual_seed(42))
+train_, validation_ = torch.utils.data.random_split(ds, [int(0.8*datasetLength), len(ds) - int(0.8*datasetLength)], generator=torch.Generator().manual_seed(42))
 dl_train = DataLoader(train_, batch_size=batch_size, num_workers=4, collate_fn=arrange_batch, shuffle=True)
 dl_val = DataLoader(validation_, batch_size=batch_size, num_workers=4, collate_fn=arrange_batch, shuffle=True)
+
+
+model = MultiImageVQA(feat_dim, vocab_size, embed_size, n_attention_stacks, hidden_dim_img)
+print(f'Loaded model weights....')
+# model.load_state_dict(torch.load('/nfs_home/janhavi2021/vqa/models/weights.pth'))
+# model = nn.DataParallel(model)
+
+criterian = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters())
+scheduler = ReduceLROnPlateau(optimizer, verbose=True, patience=6)
 
 # batch = next(dl)
 # for inx, x in enumerate(dl):
@@ -67,6 +81,7 @@ dl_val = DataLoader(validation_, batch_size=batch_size, num_workers=4, collate_f
 # dl_train, dl_val = dl, dl
 
 epoch, train_losses, val_losses, best_val_loss = 0, [], [], inf
+word_accuracy, image_accuracy = {'train': [], 'val': []}, {'train': [], 'val': []}
 for epoch in tqdm(range(num_epochs), desc=f"on epoch {epoch}"):
 
     for dataloader, phase in [(dl_train, 'train'),(dl_val, 'val')]:
@@ -76,6 +91,7 @@ for epoch in tqdm(range(num_epochs), desc=f"on epoch {epoch}"):
             model.eval()
 
         phase_loss, phase_word_loss, phase_image_loss = 0, 0, 0
+        phase_word_accuracy, phase_image_accuracy = 0, 0
         for batch in tqdm(dataloader, desc="iter"):
             optimizer.zero_grad()
             if phase == 'val':
@@ -96,6 +112,18 @@ for epoch in tqdm(range(num_epochs), desc=f"on epoch {epoch}"):
             phase_image_loss += img_classification_loss.item() * len(batch['ans'])
             phase_loss += loss.item() * len(batch['ans'])
 
+            phase_word_accuracy += accuracy_score(
+                batch['ans'].squeeze(-1).clone().detach().cpu().tolist(),
+                pred.clone().detach().cpu().tolist(),
+                normalize=False
+                )
+
+            phase_image_accuracy += accuracy_score(
+                batch["true_img_index"].squeeze(-1).clone().detach().cpu().tolist(),
+                torch.argmax(attention_weights, dim=1).clone().detach().cpu().tolist(),
+                normalize=False
+                )
+
             if phase == 'train':
                 # train_losses.append(loss.detach().cpu().item())
                 loss.backward()
@@ -106,7 +134,11 @@ for epoch in tqdm(range(num_epochs), desc=f"on epoch {epoch}"):
                 # val_losses.append()
                 # scheduler.step(loss)
 
-        tqdm.write(f'phase: {phase}, loss: {phase_loss / len(dataloader.dataset):.3f}, word_Loss: {phase_word_loss / len(dataloader.dataset):.3f}, img_classification_loss: {phase_image_loss / len(dataloader.dataset):.3f}')
+        tqdm.write(f'phase: {phase} | loss: {phase_loss / len(dataloader.dataset):.3f}, word_Loss: {phase_word_loss / len(dataloader.dataset):.3f}, img_classification_loss: {phase_image_loss / len(dataloader.dataset):.3f} | word_accuracy: {phase_word_accuracy / len(dataloader.dataset):.3f}')
+        
+        word_accuracy[phase].append(phase_word_accuracy / len(dataloader.dataset))
+        image_accuracy[phase].append(phase_image_accuracy / len(dataloader.dataset))
+
         if phase == 'train':
             train_losses.append(phase_loss / len(dataloader.dataset))
         else:
@@ -128,3 +160,12 @@ for epoch in tqdm(range(num_epochs), desc=f"on epoch {epoch}"):
     plt.xlabel('Epochs')
     plt.ylabel('Loss')
     plt.savefig('LossGraph.png')
+
+
+    with open('losses.json', 'w') as f:
+        json.dump(
+            {'word_accuracy': word_accuracy,
+            'image_accuracy': image_accuracy},
+            f,
+            indent=4
+        )
